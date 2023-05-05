87837cb28a70447d669ed9a3e8fb78f5fd2e41fd
guilherme-reale <116194356+guilherme-reale@users.noreply.github.com>
2023-05-05 14:44:31 -0300

    Add files via upload
    Main code

---
+++ sort-random.py
@@ -0,0 +1,30 @@
+import random
+
+random_list=[]
+
+for i in range(0,21):
+    random_list.append(random.randint(1,100))
+    
+print("\n--------------------Random List------------------\n")
+print(random_list)
+
+order=input("Define the sorting type (ascending or descending): ")
+
+for i in range(0,len(random_list)):
+    for j in range(0,i):
+        if order=="ascending":
+            condition=(random_list[i]<=random_list[j])
+        elif order=="descending":
+            condition=(random_list[i]>=random_list[j])
+            
+        if condition:
+            tmp=random_list[j]
+            random_list[j]=random_list[i]
+            random_list[i]=tmp
+            
+        
+    
+    i+=1
+    
+for k in range(0,len(random_list)):
+    print(random_list[k])
